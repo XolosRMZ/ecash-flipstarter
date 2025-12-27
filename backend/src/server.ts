3df2a95dfeb64f9998ee2391d35ef78ddc11dbb2
xolosarmy <contacto@xolosramirez.com>
2025-12-27 17:41:05 -0600

    fix(backend): load dotenv env vars in server startup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import app from './app';
 import { getEffectiveChronikBaseUrl } from './blockchain/ecashClient';
 import { ECASH_BACKEND, USE_CHRONIK } from './config/ecash';
